c1e3544b10e7b8ba8694a737e0c7643aed7561b8
rajavignesh23 <rajavignesh2304@gmail.com>
2024-12-13 10:42:03 +0530

    minor UI changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,6 @@ import 'package:flutter_food/pages/HomePage.dart';
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       theme:
-          ThemeData(fontFamily: 'Roboto', hintColor: const Color(0xFFd0cece)),
+          ThemeData(fontFamily: 'Ariel', hintColor: const Color(0xFFd0cece)),
       home: const HomePage(),
     ));
